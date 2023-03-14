3e4c3d2280e2487e6c7a1ef09387f69156f187f1
Pauli Anttila <pauli.anttila@gmail.com>
2023-03-14 20:22:13 +0200

    Fixed unit test

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -26,8 +26,8 @@ def test_config():
 
     assert myconfig.MQTT_BROKER_URL == "127.0.0.1"
     assert myconfig.MQTT_BROKER_PORT == 1883
-    assert myconfig.MQTT_USERNAME == ""
-    assert myconfig.MQTT_PASSWORD == ""
+    assert myconfig.MQTT_USERNAME is None
+    assert myconfig.MQTT_PASSWORD is None
     assert myconfig.MQTT_KEEPALIVE == 30
     assert myconfig.MQTT_TLS_ENABLED is False
     assert myconfig.MQTT_TLS_CA_CERTS is None
