6f90517e9a99f51bf6b54d38d27c232c1f2df27c
Pauli Anttila <pauli.anttila@gmail.com>
2023-10-14 14:56:04 +0300

    testing

--- tests/integration/mosquitto/run_mosquitto.sh
+++ tests/integration/mosquitto/run_mosquitto.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mosquitto_passwd -c tests/integration/mosquitto/mosquitto.passwd test-user test-password
+cat tests/integration/mosquitto/mosquitto.passwd
 mosquitto -c tests/integration/mosquitto/mosquitto.conf -d
