ba42c4bf7f039482e6f4a2c0affea593b12bc8eb
Pauli Anttila <pauli.anttila@gmail.com>
2023-10-14 15:35:31 +0300

    Fix paths

--- tests/integration/mosquitto/run_mosquitto.sh
+++ tests/integration/mosquitto/run_mosquitto.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "set password for testuser"
+echo "Set password for testuser"
 mosquitto_passwd -b tests/integration/mosquitto/mosquitto.passwd testuser testpassword
 echo "Run mosquitto"
 mosquitto -c tests/integration/mosquitto/mosquitto.conf &
-ps aux | grep mosquitto
